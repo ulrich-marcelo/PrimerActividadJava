eb483a1482a1957c75690945a692ad3c16b049e0
ulrich-marcelo <mulrich@estudiantes.unsam.edu.ar>
2023-09-08 17:09:55 -0300

    Parece que termine

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,5 +1,22 @@
 public class Lamparita {
     public static void main(String[] args) throws Exception {
         System.out.println("Vamos a programar una lamparita! ");
+        Lamparita pepe = new Lamparita();
+        pepe.encender();
+        System.out.println(pepe.encendida);
     }
+
+    public boolean encendida = false;
+
+    public void encender(){
+        encendida = true;
+    }
+
+    public void apagar(){
+        encendida = false;
+    }
+
+
 }
+
+
